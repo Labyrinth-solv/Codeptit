4462586620ab0aede9663bb7344e7f2cb6925c39
trungduc17732 <74634365+Labyrinth-solv@users.noreply.github.com>
2025-02-22 15:07:26 +0700

    Update b8_vn.cpp

--- b8_vn.cpp
+++ b8_vn.cpp
@@ -2,16 +2,6 @@
 
 using namespace std;
 
-bool kt(string s, int check)
-{
-	for(int i=0; i<s.size()-1; i++)
-	{
-		if(check==2&&s[i]<s[i+1]) return false;
-		else if(check==1&&s[i]>s[i+1]) return false; 
-	}
-	return true;
-}
-
 int main()
 {
 	int T; cin>>T;
